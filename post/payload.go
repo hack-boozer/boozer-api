4de72e50d72cf9ca1c4524cb05fbd92a3ece19ea
小倉裕一 <j211025ogu@gmail.com>
2018-07-14 17:38:39 +0900

    post usecaseのcreateメソッド作成

---
+++ post/payload.go
@@ -0,0 +1,11 @@
+package post
+
+import uuid "github.com/satori/go.uuid"
+
+// Create post create payload
+type Create struct {
+	AccountID uuid.UUID `json:"accountId"`
+	Comment   string    `json:"comment"`
+	Photos    []string  `json:"photos"`
+	Rate      float64   `json:"rate"`
+}
