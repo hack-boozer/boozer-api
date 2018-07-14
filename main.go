7816a5c920e2e6ba285f7bdd82292c3eac59458d
小倉裕一 <j211025ogu@gmail.com>
2018-07-14 18:09:30 +0900

    helloの削除

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	accountC "github.com/hack-boozer/boozer-api/account/controller"
 	accountR "github.com/hack-boozer/boozer-api/account/repository"
 	accountU "github.com/hack-boozer/boozer-api/account/usecase"
-	hello "github.com/hack-boozer/boozer-api/hello/controller"
 	photoR "github.com/hack-boozer/boozer-api/photo/repository"
 	postC "github.com/hack-boozer/boozer-api/post/controller"
 	postR "github.com/hack-boozer/boozer-api/post/repository"
@@ -27,11 +25,9 @@ func main() {
 	accountUse := accountU.NewAccountUsecase(accountRepo)
 	postUse := postU.NewPostUsecase(postRepo, photoRepo)
 
-	hello.NewHelloController(e)
 	accountC.NewAccountController(e, accountUse)
 	postC.NewPostController(e, postUse)
 
-	fmt.Println("vim-go")
 	port := ":" + os.Getenv("PORT")
 	e.Logger.Fatal(e.Start(port))
 }
